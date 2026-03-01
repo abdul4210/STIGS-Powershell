41711d037b2c0f5c9e1893918b94d6653c605fa9
Abdul Ali <167647128+abdul4210@users.noreply.github.com>
2026-02-28 23:50:50 -0500

    Update STIG-ID-WN11-CC-000110.ps1

--- STIG-ID-WN11-CC-000110.ps1
+++ STIG-ID-WN11-CC-000110.ps1
@@ -1,1 +1,42 @@
-# STIGS-Powershell
+<#
+.SYNOPSIS
+    This PowerShell script disables printing over HTTP.
+
+.NOTES
+    Author          : Abdul Ali
+    LinkedIn        : linkedin.com/in/abdul4210
+    GitHub          : github.com/abdul4210
+    Date Created    : 2026-02-26
+    Last Modified   : 2026-02-26
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000110
+
+.TESTED ON
+    Date(s) Tested  : 2026-02-26
+    Tested By       : Abdul Ali
+    Systems Tested  : Windows 11 pro
+    PowerShell Ver. : 
+
+.USAGE
+Run as administrator
+#>
+
+# Prevent Printing Over HTTP
+
+$RegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers"
+$ValueName = "DisableHTTPPrinting"
+$DesiredValue = 1
+
+# Ensure the registry path exists
+if (-not (Test-Path $RegistryPath)) {
+    New-Item -Path $RegistryPath -Force | Out-Null
+}
+
+# Set the required value
+New-ItemProperty -Path $RegistryPath `
+                 -Name $ValueName `
+                 -Value $DesiredValue `
+                 -PropertyType DWORD `
+                 -Force | Out-Null
