1934979825d230a65ed3e95c6eeb39dbfa3cdc62
Abdul Ali <167647128+abdul4210@users.noreply.github.com>
2026-03-05 11:36:03 -0500

    Create STIG-ID-WN11-CC-000270.ps1

---
+++ STIG-ID-WN11-CC-000270.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+    This PowerShell script prevents password saving in Remote Desktop
+
+.NOTES
+    Author          : Abdul Ali
+    LinkedIn        : linkedin.com/in/abdul4210
+    GitHub          : github.com/abdul4210
+    Date Created    : 2026-03-04
+    Last Modified   : 2026-03-04
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000270
+
+.TESTED ON
+    Date(s) Tested  : 2026-03-04
+    Tested By       : Abdul Ali
+    Systems Tested  : Windows 11 pro
+    PowerShell Ver. : 5.1
+
+.USAGE
+Run as administrator
+#>
+
+# Prevent Saving Passwords in Remote Desktop Client
+
+$RegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+$ValueName = "DisablePasswordSaving"
+$DesiredValue = 1
+
+
+# Ensure registry path exists
+if (-not (Test-Path $RegistryPath)) {
+    New-Item -Path $RegistryPath -Force | Out-Null
+}
+
+# Create or update required DWORD value
+New-ItemProperty -Path $RegistryPath `
+                 -Name $ValueName `
+                 -Value $DesiredValue `
+                 -PropertyType DWORD `
+                 -Force | Out-Null
